e4dd68de8a541ce11156eb822fb52acb77a71b78
Igor <Igorcbraz1@gmail.com>
2023-06-06 19:35:01 -0300

    :art: Sytle: add animations

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
+import { animated, useSpring, config } from '@react-spring/web'
 import {
   ArrowLeftOnRectangleIcon,
   ArrowDownTrayIcon
@@ -10,7 +11,11 @@ import { supabase } from '../service/client'
 
 import { DarkTemplate } from '../components/Templates/Dark/index.jsx'
 
+const calc = (x, y) => [-(y - window.innerHeight / 2) / 25, (x - window.innerWidth / 5) / 25, 1]
+const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
+
 export const Home = () => {
+  const [props, set] = useSpring(() => ({ xys: [0, 0, 1] , config: config.default}))
   const { dispatch } = useContext(AuthContext)
   const [user, setUser] = useState({})
   const [info, setInfo] = useState({})
@@ -254,11 +259,18 @@ export const Home = () => {
         </section>
       </header>
       <section className='flex justify-between items-center px-6 h-fit w-screen'>
-        <div className='relative'>
+        <animated.div
+          className='relative inline-block'
+          onMouseMove={({clientX: x, clientY: y}) => (set({xys: calc(x, y)}))}
+          onMouseLeave={() => set({xys:[0,0,1]})}
+          style={{
+            transform: props.xys.interpolate(trans)
+          }}
+        >
           <DarkTemplate
             username={user.user_metadata?.preferred_username}
             repositoriesNames={darkTemplateNames}
-            className='w-[480px] h-[600px]'
+            className='w-[460px] h-[570px] border-4 border-primary-300 rounded-lg shadow-lg shadow-gray-800'
           />
           <button
             className='absolute bottom-0 right-0 mr-4 mb-3 bg-white shadow-lg shadow-gray-800 text-primary-300 font-bold p-2 rounded-full transition hover:bg-primary-200 hover:text-primary-100 hover:scale-110'
@@ -276,7 +288,7 @@ export const Home = () => {
               className='w-6 h-6 pr-1'
             />
           </button> */}
-        </div>
+        </animated.div>
       </section>
     </>
   )
